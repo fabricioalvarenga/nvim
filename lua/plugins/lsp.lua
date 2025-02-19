eb48d6221d1581c364673cb6738f4280ec1b4f4c
Fabricio Alvarenga <fabriciodev@Maquina-Virtual-de-Fabricio.local>
2025-02-19 18:45:12 -0300

    Configure Pyright correctly

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,20 +2,18 @@ return {
     {
         "neovim/nvim-lspconfig",
 
-        -- Configure Pyright
-        opts = {
-            servers = {
-                pyright = {}
-            }
-        },
-
         config = function()
             local lspconfig = require("lspconfig")
+
+            -- Configure Pyright
+            lspconfig.pyright.setup({})
 
             -- Configure Swift
             lspconfig.sourcekit.setup({
                 -- Set the project's root_dir dynamically
-        		root_dir = lspconfig.util.root_pattern("*.xcodeproj", "Package.swift", ".git")(fname) or lspconfig.util.path.dirname(fname),
+                root_dir = function(fname)
+            		return lspconfig.util.root_pattern("*.xcodeproj", "Package.swift", ".git")(fname) or lspconfig.util.path.dirname(fname)
+                end,
 
                 capabilities = {
                     workspace = {
@@ -40,48 +38,44 @@ return {
             end
 
             -- Check if lua-language-server is installed
-            if is_command_available("lua-language-server") then
-                Exit_Code = 0
-            else
+            if not is_command_available("lua-language-server") then
                 io.stdout:write("lua-language-server is not installed. Installing it via homebrew...")
                 Status, Exit_Code, Code = os.execute("brew install lua-language-server")
             end
 
             -- Configure lua-language-server if it is installed
-            if Exit_Code == 0 then
-                lspconfig.lua_ls.setup({
-                    on_init = function(client)
-                        if client.workspace_folders then
-                            local path = client.workspace_folders[1].name
+            lspconfig.lua_ls.setup({
+                on_init = function(client)
+                    if client.workspace_folders then
+                        local path = client.workspace_folders[1].name
 
-                            if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-                                return
-                            end
+                        if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                            return
                         end
+                    end
 
-                        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                            runtime = {
-                                -- Tell the language server which version of Lua you're using
-                                -- (most likely LuaJIT in the case of Neovim)
-                                version = 'LuaJIT'
-                            },
-                            -- Make the server aware of Neovim runtime files
-                            workspace = {
-                                checkThirdParty = false,
-                                library = {
-                                    vim.env.VIMRUNTIME
-                                    -- Depending on the usage, you might want to add additional paths here.
-                                    -- "${3rd}/luv/library"
-                                    -- "${3rd}/busted/library",
-                                }
-                                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                                -- library = vim.api.nvim_get_runtime_file("", true)
+                    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using
+                            -- (most likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT'
+                        },
+                        -- Make the server aware of Neovim runtime files
+                        workspace = {
+                            checkThirdParty = false,
+                            library = {
+                                vim.env.VIMRUNTIME
+                                -- Depending on the usage, you might want to add additional paths here.
+                                -- "${3rd}/luv/library"
+                                -- "${3rd}/busted/library",
                             }
-                        })
-                    end,
-                    settings = { Lua = {} }
-                })
-            end
+                            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                            -- library = vim.api.nvim_get_runtime_file("", true)
+                        }
+                    })
+                end,
+                settings = { Lua = {} }
+            })
 
             vim.api.nvim_create_autocmd('LspAttach', {
                 desc = 'LSP Actions',
