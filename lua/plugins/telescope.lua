0829a4956775d10e44149c4fea3f59452dbaec44
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-05-30 14:43:41 -0300

    Configure telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,4 +5,20 @@ return {
         "nvim-lua/plenary.nvim",
         -- "nvim-telescope/telescope-file-browser.nvim",
     },
+
+    config = function()
+        local telescope = require("telescope")
+
+        telescope.setup({
+            defaults = {
+                respect_gitignore = false,
+                hidden = true,
+                grouped = true,
+                previewer = false,
+                initial_mode = "normal",
+
+            },
+        })
+    end,
+
 }
