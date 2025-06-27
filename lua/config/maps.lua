372cc869420a04405e23def0375cfb4a668c535a
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-06-27 07:54:40 -0300

    Change window resize key

--- lua/config/maps.lua
+++ lua/config/maps.lua
@@ -32,10 +32,10 @@ keymap.set("", "sj", "<C-w>j")
 keymap.set("", "sl", "<C-w>l")
 
 -- Resize window
-keymap.set("n", "<C-w><left>", "<C-w,") -- same key as "<"
-keymap.set("n", "<C-w><right>", "<C-w>.") -- same key as ">"
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
+keymap.set("n", "<C-w>,", "5<C-w><") -- same key as <
+keymap.set("n", "<C-w>.", "5<C-w>>") -- same key as >
+keymap.set("n", "<C-w>=", "5<C-w>+") -- same key as +
+keymap.set("n", "<C-w>-", "5<C-w>-")
 
 -- Map ESC key 
 keymap.set("i", "qq", "<Escape>")
